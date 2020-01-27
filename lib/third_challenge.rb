3ffd1e3cda9666ea5abf45cdfd5150cf9699fbac
Josh Couper <joshcouper@gmail.com>
2020-01-27 04:03:41 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,6 +12,7 @@ def third_challenge
       :hero => {
          :name => "Romeo",
         :age => "15"
+        :status => "alive"
         },
       :hero_friends => []
    }, 
@@ -25,7 +26,9 @@ def third_challenge
         :age => "51"
         },
       :heroine => {
-        :name => 
+        :name => "Juliet"
+        :age => "15"
+        :status => "alive"
         },
       :heroine_friends => []
    }
